58f2d4826643687dab86a8ac9df464cc2e6c9ec1
Noaozer <noao@swimm.io>
2023-05-16 16:57:55 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
         },
-        theme theme.noa themeNoa: {
+        themeeee theme.noa themeNoa: {
           customCss: require.resolve('./src/css/custom.css'),
         },
       },
